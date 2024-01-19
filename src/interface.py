de4dec4186b9e7899dd883454af65f0cb796dc1a
Pedro Neto <pedro.neto@ufrn.edu.br>
2024-01-19 18:40:28 -0300

    refc interface

--- src/interface.py
+++ src/interface.py
@@ -1,10 +1,13 @@
 import customtkinter as ctk
+from download import Download
 
 
 class App(ctk.CTk):
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
+
+        self.download_instance = Download(self)
 
         self.geometry('600x400')
         self.title('Video Downloader')
@@ -16,12 +19,12 @@ class App(ctk.CTk):
         entry_link = ctk.CTkEntry(self, width=500)
         entry_link.pack(pady=15)
 
-        button_add = ctk.CTkButton(self, text='Adicionar a fila',
-                                   height=50, width=200,)
-        button_down = ctk.CTkButton(self, text='Download',
-                                    height=50, width=200,)
-        button_add.pack(pady=10)
-        button_down.pack(pady=10)
+        button_add_video_in_queue = ctk.CTkButton(self, text='Adicionar a fila',
+                                                  height=50, width=200, command=lambda: self.download_instance.add_video_in_queue(entry_link))
+        button_download_queue = ctk.CTkButton(self, text='Download',
+                                              height=50, width=200, command=self.download_instance.download_queue)
+        button_add_video_in_queue.pack(pady=10)
+        button_download_queue.pack(pady=10)
 
 
 if __name__ == '__main__':
